a2ecf91b45c58645d1c6cbbd0d9221f1531f5c0e
Kirk Mayo <textagroup@gmail.com>
2013-08-20 07:02:10 +1200

    NEW: Adding in translations

--- code/AddImageStyle.php
+++ code/AddImageStyle.php
@@ -1,11 +1,11 @@
 <?php
 
-class AddImageStyle extends DataExtension {
+class AddImageStyleLightwindow extends DataExtension {
 	public function updateFieldsForImage($fields, $url, $file) {
 		$cssClass = $fields->fieldByName('CSSClass');
 		$items = $cssClass->getSource();
-		$items['imageholder left'] = 'Lightbox on the left, with text wrapping around';
-		$items['imageholder right'] = 'Lightbox on the right, with text wrapping around';
+		$items['imageholder left'] = _t('AlignmentStyle.LIGHTBOXLEFT', 'Lightbox on the left, with text wrapping around');
+		$items['imageholder right'] = _t('AlignmentStyle.LIGHTBOXRIGHT', 'Lightbox on the right, with text wrapping around');
 		$cssClass->setSource($items);
 		$fields->replaceField('CSSClass', $cssClass);
 	}
